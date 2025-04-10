281ec1f9d3705732fbbe0fb885869f6781cdce12
Anton Lafaev <antonlafaev@yandex.ru>
2025-04-10 09:45:08 +0300

    Commit 6. Fixture for user creation and deletion added.

--- conftest.py
+++ conftest.py
@@ -1,6 +1,9 @@
 import pytest
 import random
 
+from api_methods import ApiMethods
+
+
 @pytest.fixture(scope="function")
 def reg_values():
     user_email = f'anton_lafaev_15_{random.randint(1000,9999)}@yandex.ru'
@@ -11,3 +14,12 @@ def reg_values():
         "password": user_password,
         "name": user_name
     }
+
+@pytest.fixture(scope="function")
+def create_user_valid_data(reg_values):
+    response = ApiMethods.create_user(reg_values)
+
+    yield response
+
+    token = response.json()["accessToken"]
+    ApiMethods.delete_user(token)
