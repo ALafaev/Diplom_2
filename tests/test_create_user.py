ef31964e52184ef92b9eb9caffadb769767f7138
Anton Lafaev <antonlafaev@yandex.ru>
2025-04-10 10:53:58 +0300

    Commit 7. Parametrization for user creation tests added.

--- tests/test_create_user.py
+++ tests/test_create_user.py
@@ -1,4 +1,6 @@
 import allure
+import pytest
+
 from helpers import CheckResponse
 from data import ExpectedResponse
 from api_methods import ApiMethods
@@ -23,77 +25,24 @@ class TestCreateUser:
         assert (response.status_code == ExpectedResponse.USER_CREATION_ALSO_EXIST['status_code']
                 and response.json() == ExpectedResponse.USER_CREATION_ALSO_EXIST['response_text']), "Ответ сервера не совпадает с ожидаемым"
 
-    @allure.title('Проверка: нельзя создать пользователя, не передав значение для поля email')
-    @allure.description('Запрос POST на /api/auth/register без значения для поля email вернет 403 Forbidden')
-    def test_user_creation_without_email_value_return_403_forbidden(self, reg_values):
-        reg_data_without_email_value = {
-        "email": '',
-        "password": reg_values["password"],
-        "name": reg_values["name"]
-    }
-        response = ApiMethods.create_user(reg_data_without_email_value)
+    @allure.title('Проверка: нельзя создать пользователя, не передав значение для обязательного поля')
+    @allure.description('Запрос POST на /api/auth/register без значения для обязательного поля вернет 403 Forbidden')
+    @pytest.mark.parametrize('field_name', ['email', 'password', 'name'])
+    def test_user_creation_without_value_for_required_field_return_403_forbidden(self, reg_values, field_name):
+        reg_data_without_required_value = reg_values
+        reg_data_without_required_value[field_name] = ''
+        response = ApiMethods.create_user(reg_data_without_required_value)
 
         assert (response.status_code == ExpectedResponse.USER_CREATION_WITHOUT_REQUIRED_DATA['status_code']
                 and response.json() == ExpectedResponse.USER_CREATION_WITHOUT_REQUIRED_DATA['response_text']), "Ответ сервера не совпадает с ожидаемым"
 
-    @allure.title('Проверка: нельзя создать пользователя, не передав значение для поля password')
-    @allure.description('Запрос POST на /api/auth/register без значения для поля password вернет 403 Forbidden')
-    def test_user_creation_without_password_value_return_403_forbidden(self, reg_values):
-        reg_data_without_password_value = {
-        "email": reg_values["email"],
-        "password": '',
-        "name": reg_values["name"]
-    }
-        response = ApiMethods.create_user(reg_data_without_password_value)
 
-        assert (response.status_code == ExpectedResponse.USER_CREATION_WITHOUT_REQUIRED_DATA['status_code']
-                and response.json() == ExpectedResponse.USER_CREATION_WITHOUT_REQUIRED_DATA['response_text']), "Ответ сервера не совпадает с ожидаемым"
-
-    @allure.title('Проверка: нельзя создать пользователя, не передав значение для поля name')
-    @allure.description('Запрос POST на /api/auth/register без значения для поля name вернет 403 Forbidden')
-    def test_user_creation_without_name_value_return_403_forbidden(self, reg_values):
-        reg_data_without_name_value = {
-        "email": reg_values["email"],
-        "password": reg_values["password"],
-        "name": ''
-    }
-        response = ApiMethods.create_user(reg_data_without_name_value)
-
-        assert (response.status_code == ExpectedResponse.USER_CREATION_WITHOUT_REQUIRED_DATA['status_code']
-                and response.json() == ExpectedResponse.USER_CREATION_WITHOUT_REQUIRED_DATA['response_text']), "Ответ сервера не совпадает с ожидаемым"
-
-    @allure.title('Проверка: нельзя создать пользователя при отсутствии в запросе поля email')
-    @allure.description('Запрос POST на /api/auth/register без поля email вернет 403 Forbidden')
-    def test_user_creation_without_email_field_return_403_forbidden(self, reg_values):
-        reg_data_without_email_field = {
-        "password": reg_values["password"],
-        "name": reg_values["name"]
-    }
-        response = ApiMethods.create_user(reg_data_without_email_field)
-
-        assert (response.status_code == ExpectedResponse.USER_CREATION_WITHOUT_REQUIRED_DATA['status_code']
-                and response.json() == ExpectedResponse.USER_CREATION_WITHOUT_REQUIRED_DATA['response_text']), "Ответ сервера не совпадает с ожидаемым"
-
-    @allure.title('Проверка: нельзя создать пользователя при отсутствии в запросе поля password')
-    @allure.description('Запрос POST на /api/auth/register без поля password вернет 403 Forbidden')
-    def test_user_creation_without_password_field_return_403_forbidden(self, reg_values):
-        reg_data_without_password_field = {
-        "email": reg_values["email"],
-        "name": reg_values["name"]
-    }
-        response = ApiMethods.create_user(reg_data_without_password_field)
-
-        assert (response.status_code == ExpectedResponse.USER_CREATION_WITHOUT_REQUIRED_DATA['status_code']
-                and response.json() == ExpectedResponse.USER_CREATION_WITHOUT_REQUIRED_DATA['response_text']), "Ответ сервера не совпадает с ожидаемым"
-
-    @allure.title('Проверка: нельзя создать пользователя при отсутствии в запросе поля name')
-    @allure.description('Запрос POST на /api/auth/register без поля name вернет 403 Forbidden')
-    def test_user_creation_without_name_field_return_403_forbidden(self, reg_values):
-        reg_data_without_name_field = {
-        "email": reg_values["email"],
-        "password": reg_values["password"]
-    }
-        response = ApiMethods.create_user(reg_data_without_name_field)
+    @allure.title('Проверка: нельзя создать пользователя при отсутствии в запросе обязательного поля')
+    @allure.description('Запрос POST на /api/auth/register без обязательного поля вернет 403 Forbidden')
+    @pytest.mark.parametrize('field_name', ['email', 'password', 'name'])
+    def test_user_creation_without_required_field_return_403_forbidden(self, reg_values, field_name):
+        reg_data_without_required_field = reg_values.pop(field_name)
+        response = ApiMethods.create_user(reg_data_without_required_field)
 
         assert (response.status_code == ExpectedResponse.USER_CREATION_WITHOUT_REQUIRED_DATA['status_code']
                 and response.json() == ExpectedResponse.USER_CREATION_WITHOUT_REQUIRED_DATA['response_text']), "Ответ сервера не совпадает с ожидаемым"
